174b42b4f7bacf771fed185c868740ca7a4137ce
VinVinnieVince <vincent.chan@hotmail.co.uk>
2024-08-25 14:46:05 +0100

    Add function to display books on page as 'cards'
     - Add code to create div libraryContainer which will contain the book list
     - Create function displaybooks() to add books to page
     - call function on line 67 for testing purposes, will be removed in future
     - console.table() on line 63 for testing purposes

--- script/library.js
+++ script/library.js
@@ -35,3 +35,33 @@ exampleBook = new Book (
     'George R. R. Martin', 
     '???');
 library.push(exampleBook);
+
+const body = document.querySelector('body');
+const libraryContainer = document.createElement('div');
+
+body.appendChild(libraryContainer);
+
+function displayBooks () {
+    libraryContainer.textContent = '';
+
+    for (book in library) {
+        const bookContainer = document.createElement('div');
+        // h1 for website titles, h2 for sub-headings, hence h3 for book titles
+        const bookTitle = document.createElement('h3');
+        const bookAuthor = document.createElement('p');
+        const bookPages = document.createElement('p');
+
+        bookTitle.textContent = library[book].title;
+        bookAuthor.textContent = library[book].author;
+        bookPages.textContent = 'Pages: ' + library[book].pages;
+
+        libraryContainer.appendChild(bookContainer);
+        bookContainer.appendChild(bookTitle);
+        bookContainer.appendChild(bookAuthor);
+        bookContainer.appendChild(bookPages);
+
+        console.table(library[book]);
+    }
+}
+
+displayBooks();
